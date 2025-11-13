c7cdc02dbe671babc1d676c3cad8b74d0c1e2d7c
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-13 06:08:10 +0100

    calculation of energy

---
+++ CalcEnergy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CalcEnergy{
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        
+    System.out.print("Enter the number in feet that you want to convert to meter: ");
+    double feetNum = input.nextDouble();
+
+     
+    }
+}
