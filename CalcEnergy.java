efed9ebc4f273d484145f98fec4adb54f12a17fd
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-13 06:40:27 +0100

    Add water energy calculation to CalcEnergy

--- CalcEnergy.java
+++ CalcEnergy.java
@@ -3,8 +3,19 @@ public class CalcEnergy{
 public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
         
-    System.out.print("Enter the number in feet that you want to convert to meter: ");
-    double feetNum = input.nextDouble();
+    System.out.print("Enter the amount of water in kilogram: ");
+    double waterKg = input.nextDouble();
+
+    System.out.print("Enter initial water temperature in degree celcious: ");
+    double temp1 = input.nextDouble();
+
+    System.out.print("Enter final water temperature in degree celcious: ");
+    double temp2 = input.nextDouble();
+
+    double energy = waterKg * (temp2 - temp1) * 4184;
+
+
+    System.out.println("The energy needed is: " + energy);
 
      
     }
